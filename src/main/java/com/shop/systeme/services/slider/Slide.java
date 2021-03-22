21bc1722eddc23b2f6e4314e84dbc0f24724dfe3
ruspi <rus.pipan@ukr.net>
2021-03-22 19:07:31 +0200

    Slide.

---
+++ src/main/java/com/shop/systeme/services/slider/Slide.java
@@ -0,0 +1,50 @@
+package com.shop.systeme.services.slider;
+
+public class Slide {
+    private String layer1;
+    private String layer2;
+    private String layer3;
+    private String img;
+    private int positions;
+
+
+    public String getLayer1() {
+        return layer1;
+    }
+
+    public void setLayer1(String layer1) {
+        this.layer1 = layer1;
+    }
+
+    public String getLayer2() {
+        return layer2;
+    }
+
+    public void setLayer2(String layer2) {
+        this.layer2 = layer2;
+    }
+
+    public String getLayer3() {
+        return layer3;
+    }
+
+    public void setLayer3(String layer3) {
+        this.layer3 = layer3;
+    }
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
+
+    public int getPositions() {
+        return positions;
+    }
+
+    public void setPositions(int positions) {
+        this.positions = positions;
+    }
+}
