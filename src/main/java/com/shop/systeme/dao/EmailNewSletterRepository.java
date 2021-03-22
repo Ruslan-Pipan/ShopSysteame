1a65705d5ec76efc5dfe04e71fa2bf4659705949
ruspi <rus.pipan@ukr.net>
2021-03-22 18:59:35 +0200

    This repository for email.

---
+++ src/main/java/com/shop/systeme/dao/EmailNewSletterRepository.java
@@ -0,0 +1,7 @@
+package com.shop.systeme.dao;
+
+import com.shop.systeme.model.EmailConsumersHoWantResetEmail;
+import org.springframework.data.repository.CrudRepository;
+
+public interface EmailNewSletterRepository extends CrudRepository<EmailConsumersHoWantResetEmail,String> {
+}
