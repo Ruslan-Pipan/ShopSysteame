26af2729ee249bce2805f29f10e7ad54c14672fc
ruspi <rus.pipan@ukr.net>
2021-03-22 19:10:36 +0200

    Represent last items.

---
+++ src/main/java/com/shop/systeme/services/items/LastItems.java
@@ -0,0 +1,39 @@
+package com.shop.systeme.services.items;
+
+import com.shop.systeme.dao.items.ItemsRepository;
+import com.shop.systeme.model.iteams.Item;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.util.Collections;
+import java.util.List;
+
+@Component("lastItems")
+public class LastItems implements ListItems{
+
+    private List<Item> items;
+
+    @Autowired
+    private ItemsRepository itemsRepository;
+
+    @Override
+    public List<Item> getItems() {
+        return Collections.unmodifiableList(items);
+    }
+
+    @Override
+    public void addItem(Item item) {
+        items.add(item);
+    }
+
+    @Override
+    public void deleteItem(Item item) {
+        items.remove(item);
+    }
+
+    @PostConstruct
+    private void initMethod(){
+        items = itemsRepository.findFirst9ByOrderByIdDesc();
+    }
+}
