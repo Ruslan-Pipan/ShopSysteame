7a2f84c384d53d5b2eed4502e262aed6dd9cbd90
ruspi <rus.pipan@ukr.net>
2021-03-22 19:08:01 +0200

    Minor changes.

--- src/main/java/com/shop/systeme/ShopApplication.java
+++ src/main/java/com/shop/systeme/ShopApplication.java
@@ -2,13 +2,23 @@ package com.shop.systeme;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-public class ShopApplication {
+@EnableAsync
+@EnableScheduling
+public class ShopApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
-
 		SpringApplication.run(ShopApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder app){
+		return app.sources(ShopApplication.class);
 	}
 
 }
